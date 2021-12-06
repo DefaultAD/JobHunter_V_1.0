d5e2f03adfa4277aaab79859663ac8c00ee4d36e
DefaultAD <dewaldtdevilliers@gmail.com>
2021-12-06 16:00:06 +0200

    Small Map Fix and Outline Shader
    Removed walls and pickups. added outline shader to pickups

--- Assets/Scripts/PickUpColor.cs
+++ Assets/Scripts/PickUpColor.cs
@@ -16,6 +16,8 @@ public class PickUpColor : MonoBehaviour
     public bool score1PickUpCheck;
     public bool score2PickUpCheck;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
